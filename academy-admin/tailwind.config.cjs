1524def72f8f056acd6796f9997203ac848ffbe3
unknown <thanish7442@gmail.com>
2025-11-23 00:23:48 +0530

    fix(tailwind): add tailwind + postcss configs and postinstall build

---
+++ academy-admin/tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    '../**/*.{ejs,html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
